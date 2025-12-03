303e59398f93f6f2d27fe0d2fff9680a1410bb84
T4t4n32 <T4t4n32@users.noreply.github.com>
2025-12-03 16:54:35 -0500

    LoRa_Test

---
+++ HARDWARE /LoRa/meshtastic_wifi_lora_32_V3/flash.bat
@@ -0,0 +1,1 @@
+esptool.exe --chip esp32s3 --port COM4 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 8MB 0x0000 bootloader.bin 0x8000 partitions.bin 0xe000 boot_app0.bin 0x10000 firmware.bin
