db3e367e1c9a63162bb80b08973073fbd280ca76
T4t4n32 <T4t4n32@users.noreply.github.com>
2025-12-03 16:54:35 -0500

    LoRa_Test

---
+++ HARDWARE /LoRa/Lora32_V3_factest_HF/Lora32_V3_HF/Lora32_V3_HF.bat
@@ -0,0 +1,1 @@
+"esptool.exe" --chip esp32s3 --port "COM4" --baud 921600  --before default_reset --after hard_reset write_flash  -z --flash_mode keep --flash_freq keep --flash_size keep 0x0 "Lora32_V3_HF.ino.bootloader.bin" 0x8000 "Lora32_V3_HF.ino.partitions.bin" 0xe000 "boot_app0.bin" 0x10000 "Lora32_V3_HF.ino.bin" 
